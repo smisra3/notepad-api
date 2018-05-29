77d35107b9386137209879650eae9cf6ff3d926a
SAPIENT\smis64 <smisra3@sapient.com>
2018-05-29 16:48:42 +0530

    Changes for heroku

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongo = require('mongodb');
 const mongoClient = mongo.MongoClient;
-const url = "mongodb://localhost:27017/Tutorial";
+const url = "mongodb://heroku_m6js5kv3:4hg0oeu6b7kh4uq1g2lt06bdbs@ds237660.mlab.com:37660/heroku_m6js5kv3" || "mongodb://localhost:27017/Tutorial";
+const port = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -47,4 +48,4 @@ app.get('/tasks', function (req, res) {
     });
 });
 
-app.listen(3000);
+app.listen(port);
