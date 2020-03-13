be6b5bf849dec745bd84a20202040a2bc5669c50
Shubhankar Misra <shubhankar.misra@99Acres.com>
2020-03-13 16:20:26 +0530

    Saving work

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongo = require('mongodb');
 const mongoClient = mongo.MongoClient;
-const url = "mongodb://localhost:27017/Tutorial";
+const url = "mongodb://127.0.0.1:27017/Tutorial";
 const port = 3001;
 
 app.use('/', express.static(__dirname + '/'));
@@ -16,6 +16,21 @@ app.get('/tasks', (req, res) => {
   mongoClient.connect(url, async (error, db) => {
     if (!error) {
       const results = await db.collection('tasks').find({}).toArray();
+      res.send(JSON.stringify(results));
+      res.end();
+    } else console.log('error while getting tasks from DB :', error);
+  });
+});
+
+app.get('/:id', (req, res, next) => { // this to be used by user
+  if (!req.params.id) {
+    const aaa = Math.random();
+    res.status(300).redirect(`/${aaa}`);
+  }
+  mongoClient.connect(url, async (error, db) => {
+    if (!error) {
+      const predicate = req.params.id ? { description: req.params.id } : {};
+      const results = await db.collection('tasks').find(predicate).toArray();
       res.send(JSON.stringify(results));
       res.end();
     } else console.log('error while getting tasks from DB :', error);
@@ -46,6 +61,10 @@ app.post('/tasks', (req, res, next) => {
       });
     } else console.log(error);
   });
+});
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
 });
 
 app.get('*', (req, res) => {
